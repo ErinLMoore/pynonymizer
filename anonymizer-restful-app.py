1c172ab34353efafb4beddce206e68217137fa8e
Erin Moore <emoore@pillartechnology.com>
2016-09-19 16:59:18 -0400

    IT WORKS

--- anonymizer-restful-app.py
+++ anonymizer-restful-app.py
@@ -8,7 +8,13 @@ import pygit2 as git
 import os
 import shutil
 import sys
+import time
+import datetime
+import hashlib
 
+import requests
+
+from set_credentials import git_username, git_password, git_token
 
 app = Flask(__name__)
 api = Api(app)
@@ -38,6 +44,13 @@ def copy_directory_except_git(src, dst):
 def delete_directory(src):
     shutil.rmtree(src, ignore_errors = True)
 
+def create_github_repo(new_repo_name):
+    dictToSend = {"name": new_repo_name}
+    headers = {'Authorization': 'token {0}'.format(git_token)}
+    res = requests.post('https://api.github.com/user/repos', json=dictToSend, headers=headers)
+    print ('response from server:',res.text)
+    dictFromServer = res.json()
+
 class AnonymizeAPI(Resource):
     def __init__(self):
         self.reqparse = reqparse.RequestParser()
@@ -49,20 +62,27 @@ class AnonymizeAPI(Resource):
 
     def post(self):
         request = self.reqparse.parse_args()
-
+        anonymous_name = self.get_anonymous_name(request)
+        create_github_repo(anonymous_name)
         anonymizer = Anonymizer(request)
-        anonymizer.anonymize()
+        anonymizer.anonymize(anonymous_name)
 
         return anonymizer.get_result()
+
+    def get_anonymous_name(self, request):
+        tmp = str(time.time())
+        epoch = tmp.encode()
+        myhash = hashlib.md5(epoch).hexdigest()
+        parsed_description = request.description.replace(" ", "-")
+        return parsed_description + "_" + str(datetime.date.today()) + "_" + myhash[:5]
 
 class Anonymizer(object):
     def __init__(self, request):
         self.request = request
         self.result = {}
-        from set_credentials import git_username, git_password
         self.username, self.password = git_username, git_password
 
-    def anonymize(self):
+    def anonymize(self, anonymous_name):
         cred= git.UserPass(self.username, self.password)
         callbacks = git.RemoteCallbacks(credentials = cred)
         git.clone_repository(self.request.url, "./local_copy", bare=False, callbacks = callbacks)
@@ -70,25 +90,32 @@ class Anonymizer(object):
 
         git.init_repository("./anonymous_copy", flags = git.GIT_REPOSITORY_INIT_MKDIR)
 
+        committer = git.Signature('Anonymous', 'anonymous@example.com')
+        author = git.Signature('Anonymous', 'anonymous@example.com')
         parent = []
+        counter = 0
         for commit in original_repo.walk(original_repo.head.target, git.GIT_SORT_TOPOLOGICAL):
-            print (commit.tree_id)
             original_repo.checkout_tree(treeish = commit, strategy = git.GIT_CHECKOUT_ALLOW_CONFLICTS)
             copy_directory_except_git("./local_copy", "./anonymous_copy")
             copy_repo = git.Repository("./anonymous_copy/.git")
-            author = git.Signature('Alice Author', 'alice@authors.tld')
-            committer = git.Signature('Cecil Committer', 'cecil@committers.tld')
+
             tree = copy_repo.TreeBuilder().write()
             copy_repo.create_commit(
             'refs/heads/master', # the name of the reference to update
-            author, committer, 'one line commit message\n\ndetailed commit message',
-            tree, # binary string representing the tree object ID
+            author, committer, 'commit #' + str(counter),
+            tree,
             parent
             )
             parent = [copy_repo.head.get_object().hex]
-            #raise GitError(message)
+            counter += 1
+
+        original_repo.remotes.set_url("origin", "https://github.com/{0}/{1}.git".format(self.username, anonymous_name))
+        remote = original_repo.remotes["origin"]
+        remote.push(['refs/heads/master:refs/heads/master'], callbacks = callbacks)
+        
+        self.result = {'anonymous_url': anonymous_name, 'success': True}
         delete_directory("./local_copy")
-        self.result = {'anonymous_url': 'shasdflk', 'success': True}
+        delete_directory("./anonymous_copy")
 
     def get_result(self):
         return self.result
